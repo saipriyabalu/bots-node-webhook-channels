7ef5108f107c3ce684f01cd82b6a3a7dee79657a
Saipriya <saipriyabalu@yahoo.co.in>
2020-06-25 18:07:20 -0500

    new update

--- googleactions/service.js
+++ googleactions/service.js
@@ -1,7 +1,5 @@
 const OracleBot = require('@oracle/bots-node-sdk');
 const { WebhookClient, WebhookEvent } = OracleBot.Middleware;
-const bodyParser = require('body-parser')
-
 
 module.exports = (app) => {
   const logger = console;
@@ -13,13 +11,9 @@ module.exports = (app) => {
 
   const webhook = new WebhookClient({
     channel: {
-    //   url: 'https://botv2lhr1I0007HFB3E3Abots-mpaasocimt.botmxp.ocp.oraclecloud.com:443/connectors/v1/tenants/idcs-6d466372210e4300bb31f4db15e8e96c/listeners/webhook/channels/fb20aa74-2b90-483e-82f2-412f84f35861',
-    //   secret: 'PKrBKIlpyhiHHeSgWasbPAHG7sepCLWu'
-
         url: 'https://oda-a31ecd9faf074469bdb8eae2d59314aa-da2.data.digitalassistant.oci.oraclecloud.com/connectors/v2/listeners/webhook/channels/e3df1af8-136f-48f9-a23c-60a57a9c0d0b',
         secret: 'G2vb6MBGUGQMlzhwoeOWK97cYvQjkG3e'
-
-    }
+}
   });
 
   webhook
@@ -31,49 +25,43 @@ module.exports = (app) => {
   function assistantMessage(request) {
     return new Promise(function (resolve, reject) {
       try {	
-		function getMsg() {
-			let input = request.inputs[0].rawInputs[0].query;
-			const intent = request.inputs[0].intent;
-			const conversationType = request.conversation.type;
+		let input = request.inputs[0].rawInputs[0].query;
+		const intent = request.inputs[0].intent;
 
-			expectUserResponse = true;
-			if (intent === 'actions.intent.CANCEL') {
-			  expectUserResponse = false;
-			}
-			
-			const MessageModel = webhook.MessageModel();
-			const message = {
-//			  userId: randomIntInc(1000000, 9999999).toString(), //'anonymous', 
-			  userId: 'anonymous', 
-			  messagePayload: MessageModel.textConversationMessage(input)
-			};
-			webhook.send(message);
-			webhook.on(WebhookEvent.MESSAGE_RECEIVED, message => {
-			  resolve(message);
-			});
+		expectUserResponse = true;
+		if (intent === 'actions.intent.CANCEL') {
+			expectUserResponse = false;
 		}
-		setTimeout(getMsg, 3000);			
+		
+		const MessageModel = webhook.MessageModel();
+		const message = {
+			userId: 'anonymous',//randomIntInc(1000000, 9999999).toString(), //'anonymous', 
+			messagePayload: MessageModel.textConversationMessage(input)
+		};
+		webhook.send(message);
+		webhook.on(WebhookEvent.MESSAGE_RECEIVED, message => {
+			console.log('Message Received: ' + JSON.stringify(message));
+			resolve(message);
+		});
       } catch (err) {
         console.error(err);
         reject(err);
       }
     })
   }
-
-  // new
   
   app.post('/bot/message', webhook.receiver());
 
   app.get('/', (req, res) => res.send('Oracle Digital Assistant for Google Assistant app is running.'));
   
-  app.post('/user/message', function(args, res) {
+  app.post('/user/message', function(args, res, next) {
     const request = args.body;
     console.log(JSON.stringify(request, null, 2));
-    assistantMessage(request)
-    .then(function (result) {
+    assistantMessage(request).then(function (result) {
 	  res.setHeader('Content-Type', 'application/json');
 	  res.append('Google-Assistant-API-Version', 'v2');
 	  res.json(formatResponse(result.messagePayload.text));
+	  next();
     })
 	.catch(function(err) {
 	  console.error('Error: ' + err);
